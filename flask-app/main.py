073df094b50fa94a12c5d9b302ba00fdc51fbb90
Avikant Srivastava <asdavikant@gmail.com>
2019-09-25 09:12:44 +0530

    front end improvements

--- flask-app/main.py
+++ flask-app/main.py
@@ -1,16 +1,43 @@
+#import part
+
 from flask import *
+import numpy as np
+import os
+import sys
 import geocoder
 import reverse_geocoder as rg
+import requests
+from flask import jsonify
+# import matplotlib.pyplot as plt
+import pandas as pd
 import folium
+from folium import FeatureGroup, LayerControl, Map, Marker
+from folium.plugins import MarkerCluster
+from folium import plugins
+from sklearn.cluster import KMeans
+
 app = Flask(__name__)
 
-title_data = ['null' , 'F1', 'F2', 'F3', 'F4', 'F5', 'F6', 'F7']
+
+#data part
+
+title_data = ['null' , 'F1', 'F2', 'F3', 'F4', 'Number of Trees Burned Over The Year', 'User Reports vs Validation', 'F7']
 
 data_entry = ['data_element1', 'data_element2', 'data_element3', 'data_element4']
 
-f5 = [5000, 2000, 6000, 4000, 5000, 6000, 1000, 8000]
+f5 = [2728, 823, 1621, 1863, 2483, 2975, 3495, 4247 , 6741 , 5248 , 4639 , 3146]
 
 f7 = ['F7_Title1', 'F7_Title2', 'F7_Title3', 'F7_Title4', 'F7_Title5']
+
+start_coords = [13.148, 77.611]
+
+circle = [87.475 , 5.273, 7.252]
+
+
+#custom defined functions
+
+
+
 
 def name_from_listdict(ld):
     name = ld[0]['name'] + " " + ld[0]['admin2'] + " " + ld[0]['admin1']
@@ -28,29 +55,33 @@ def geo_search(coordinates):
 # name = name_from_listdict(l_d)
 
 
-@app.route('/')
-def homepage():
-    return render_template('index.html' , data = title_data , f5 = f5 , f7 = f7 , data_entry = data_entry)
 
-# @app.route('/world')
-# def homepage():
-#     return render_template('world-wind.html')
+
+#webpage part
+
+@app.route('/')
+def index():
+    return render_template('index.html' , data = title_data , f5 = f5 , f7 = f7 , data_entry = data_entry , circle = circle)
+
+
+
+
+
+@app.route('/form')
+def form():
+    return render_template('profile.html')
+
+@app.route('/d3js1')
+def d3js1():
+    return render_template('d3js1.html')
+
+@app.route("/get_my_ip", methods=["GET"])
+def get_my_ip():
+    return jsonify({'ip': request.remote_addr}), 200
+
+
+
 
 @app.route('/map')
-# def index():
-#     start_coords = (46.9540700, 142.7360300)
-#     folium_map = folium.Map(location=start_coords, zoom_start=14)
-#     # folium_map.save()
-#     return render_template('folium.html', folium_map=folium_map)
-
 def map():
-    start_coords = [13.148, 77.611]
-    folium_map = folium.Map(location=start_coords,zoom_start=14,
-                        tiles='openstreetmap')
-    tooltip = 'Danger Khatra !'
-    name = geo_search(start_coords)
-    # {{ name[0] }}
-    folium.Marker(start_coords, popup= name, tooltip=tooltip).add_to(folium_map)
-    # folium.Marker( location = start_coords , popup = "Danger !" ,  (fill_color='#43d9de', radius = 8) ).add_to(folium_map)
-    folium_map.save('templates/map.html')
-    return render_template('folium.html')
+    return render_template('osm4.html')
